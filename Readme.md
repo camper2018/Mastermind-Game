dfd963e0ec3111256fa0ce6bca7e86fa9977ee10
camper2018 <Taniafali@gmail.com>
2022-05-19 22:31:42 -0700

    Add some functions and implement axios use

---
+++ Readme.md
@@ -0,0 +1,14 @@
+## Mastermind Game
+
+This game was built with HTML, CSS, Javascript.
+
+# Assisting libraries
+
+1. Jquery for assisting DOM manipulation
+2. Bootstrap for layout.
+
+## How to run the project
+
+1. Clone the project on your local machine.
+2. In your terminal, navigate to the project's root directory and type npm install or yarn install to install all project's dependencies. You might need to download Node.js if you don't have it already and it will automatically install npm. For yarn you need to type the following command in the terminal: npm install --global yarn.
+3. To run the project, type npm run dev in your terminal. Then navigate to [http://localhost:1234/] with your browser.
--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+// to allow async await using axios
+import "regenerator-runtime/runtime";
+import axios from "axios";
+const getRandomColorSequence = (sequence) => {
+  const colors = [
+    "#ff0000ff",
+    "#ff9900ff",
+    "#ffff00ff",
+    "#6aa84fff",
+    "#0000ffff",
+    "#9900ffff",
+    "#ff00ffff",
+    "#85200cff",
+  ];
+  const colorSequence = sequence.map((number, i) => {
+    return colors[Number(number)];
+  });
+  return colorSequence;
+};
+const generateRandomSequence = async (num, max) => {
+  try {
+    const response = await axios.get(
+      `https://www.random.org/integers/?num=${num}&min=0&max=${max}&col=1&base=10&format=plain&rnd=new`
+    );
+
+    const randomSequence = response.data;
+
+    console.log(randomSequence);
+    return randomSequence;
+  } catch (errors) {
+    console.error(errors);
+  }
+};
+const getPlacementsCount = (str) => {
+  let nums = [];
+  let num = 0;
+  if (str.toLowerCase() === "easy") {
+    num = 3;
+  } else if (str.toLowerCase() === "medium") {
+    num = 4;
+  } else if (str.toLowerCase() === "hard") {
+    num = 5;
+  }
+  while (num > 0) {
+    nums.push(num);
+    num--;
+  }
+  return nums;
+};
 const initGame = () => {};
 
-$("document").ready(() => {});
+$(document).ready(() => {
+  generateRandomSequence(4, 7);
+});
